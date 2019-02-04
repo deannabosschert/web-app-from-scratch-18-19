d028fce04db90cf23e5d9638d81009c3354bf335
deannabosschert <deanna.bosschert@gmail.com>
2019-02-04 16:21:47 +0100

    Generate list of images in index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,24 @@
-var request = new XMLHttpRequest();
-var apiLink = 'https://api.unsplash.com/photos/?client_id=52ed480c90eaf026bd329345fd21431bdec643724f11f8aa54b49a48453edf8e';
+var request = new XMLHttpRequest()
+var apiLink = 'https://api.unsplash.com/photos/?client_id=52ed480c90eaf026bd329345fd21431bdec643724f11f8aa54b49a48453edf8e'
+var element = document.getElementById("root")
 
-request.open('GET', apiLink , true);
+
+request.open('GET', apiLink , true)
 
 request.onload = function () {
 
-  var data = JSON.parse(this.response);
+  var data = JSON.parse(this.response)
 
-  localStorage.setItem('images', JSON.stringify(data));
-
+  localStorage.setItem('images', JSON.stringify(data))
 
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(image => {
-      console.log(image.urls.regular);
+    data.forEach(imageUnsplash => {
+      element.innerHTML+= '<img src="'+imageUnsplash.urls.regular+'">'
+      console.log(imageUnsplash.urls.regular)
     });
   } else {
-    console.log('error');
+    console.log('error')
   }
 }
 
-request.send();
+request.send()
