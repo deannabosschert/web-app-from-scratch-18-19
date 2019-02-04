6ea2daf638c2f565aa2f3242a77c863baacc5e7b
deannabosschert <deanna.bosschert@gmail.com>
2019-02-04 15:36:32 +0100

    parse JSON and save in LocalStorage

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ request.onload = function () {
 
   var data = JSON.parse(this.response);
 
+  localStorage.setItem('colors', JSON.stringify(data));
+
+
   if (request.status >= 200 && request.status < 400) {
     data.forEach(image => {
       console.log(image.color);
