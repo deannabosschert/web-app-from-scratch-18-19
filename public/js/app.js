009788c462100fe1420b4253d956bda3204740ac
deannabosschert <deanna.bosschert@gmail.com>
2019-02-04 15:10:08 +0100

    load data from API

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
-'use strict'
-https://unsplash.com/collections/3178572/winter
+var request = new XMLHttpRequest();
+var apiLink = 'https://api.unsplash.com/photos/?client_id=52ed480c90eaf026bd329345fd21431bdec643724f11f8aa54b49a48453edf8e';
+
+request.open('GET', apiLink , true);
+
+request.onload = function () {
+
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(image => {
+      console.log(image.color);
+    });
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
